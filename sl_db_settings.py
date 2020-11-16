605a3ab2c6f7ef459a26b1726d93180ae20f2d63
Interligo <interligo@yandex.ru>
2020-11-16 13:49:11 +0300

    added db_settings

---
+++ sl_db_settings.py
@@ -0,0 +1,6 @@
+URL = "https://shoppinglist-c382.restdb.io/rest/shoppinglist?q={}&sort=Category&dir=1"
+HEADERS = {
+    'content-type': "application/json",
+    'x-apikey': "acdc84186c4184fd103c4918fae744d0a7b40",
+    'cache-control': "no-cache"
+    }
