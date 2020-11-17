36dae3adad26d9760ecfe410a54e078eda2268e1
Interligo <interligo@yandex.ru>
2020-11-17 16:51:20 +0300

    added search url to unify search

--- sl_db_settings.py
+++ sl_db_settings.py
@@ -1,6 +1,7 @@
-URL = "https://shoppinglist-c382.restdb.io/rest/shoppinglist?q={}&sort=Category&dir=1"
+URL = 'https://shoppinglist-c382.restdb.io/rest/shoppinglist?q={}&sort=Category&dir=1'
+SEARCH_URL = 'https://shoppinglist-c382.restdb.io/rest/shoppinglist?q='
 HEADERS = {
-    'content-type': "application/json",
-    'x-apikey': "acdc84186c4184fd103c4918fae744d0a7b40",
-    'cache-control': "no-cache"
+    'content-type': 'application/json',
+    'x-apikey': 'acdc84186c4184fd103c4918fae744d0a7b40',
+    'cache-control': 'no-cache'
     }
